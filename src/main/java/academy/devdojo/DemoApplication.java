625c2bdc23d8fe659a1504ccf0756e28cffe7186
jader1948 <J@der1948>
2023-11-16 18:47:12 -0300

    video06 :  demo application, conponetScan

--- src/main/java/academy/devdojo/DemoApplication.java
+++ src/main/java/academy/devdojo/DemoApplication.java
@@ -2,15 +2,16 @@ package academy.devdojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		var applicationContext = SpringApplication.run(DemoApplication.class, args);
+		Arrays.stream(applicationContext.getBeanDefinitionNames())
+				.forEach(System.out::println);
 	}
-
 }
-
-
-
